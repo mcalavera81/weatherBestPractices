611f2109f042d5fda2cfdc1e99af20c3e99b5eea
mcalavera81 <farid.hafhafi@fonetic.com>
2017-06-10 13:15:52 +0200

    initial commit

---
+++ src/main/java/com/crossover/trial/weather/AirportLoader.java
@@ -0,0 +1,46 @@
+package com.crossover.trial.weather;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import java.io.*;
+
+/**
+ * A simple airport loader which reads a file from disk and sends entries to the webservice
+ *
+ * TODO: Implement the Airport Loader
+ * 
+ * @author code test administrator
+ */
+public class AirportLoader {
+
+    /** end point to supply updates */
+    private WebTarget collect;
+
+    public AirportLoader() {
+        Client client = ClientBuilder.newClient();
+        /* end point for read queries */
+        WebTarget query = client.target("http://localhost:8080/query");
+        collect = client.target("http://localhost:8080/collect");
+    }
+
+    public void upload(InputStream airportDataStream) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(airportDataStream));
+        String l = null;
+        while ((l = reader.readLine()) != null) {
+            break;
+        }
+    }
+
+    public static void main(String args[]) throws IOException{
+        File airportDataFile = new File(args[0]);
+        if (!airportDataFile.exists() || airportDataFile.length() == 0) {
+            System.err.println(airportDataFile + " is not a valid input");
+            System.exit(1);
+        }
+
+        AirportLoader al = new AirportLoader();
+        al.upload(new FileInputStream(airportDataFile));
+        System.exit(0);
+    }
+}
